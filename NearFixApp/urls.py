3d52695e3ce24c9314b333393aba7a3b5a4628a1
Gaurav Sawant <gauravsawant8217@gmail.com>
2026-01-07 20:13:20 +0530

    fixed changes

--- NearFixApp/urls.py
+++ NearFixApp/urls.py
@@ -3,12 +3,6 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-<<<<<<< HEAD
-    path("", views.index, name="index"),
-
-    # AUTH
-    path("login", views.login, name="login")
-=======
-    path("", views.home, name="index")
->>>>>>> 2ff8f80a243a6b97ee6899930bff1bc40ecd0eee
+    path("", views.home, name="home"),
+    path("/login", views.login, name="login")
 ]
--- NearFixApp/views.py
+++ NearFixApp/views.py
@@ -2,16 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-<<<<<<< HEAD
-def index(request):
-  return render(request, "index.html")
+def home(request):
+  return render(request, "home.html")
 
 def login(request):
   return render(request, "login.html")
 
 def register(request):
   return render(request, "register.html")
-=======
-def home(request):
-  return render(request, "home.html")
->>>>>>> 2ff8f80a243a6b97ee6899930bff1bc40ecd0eee
