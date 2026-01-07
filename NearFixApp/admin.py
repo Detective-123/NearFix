37a652bdce839bfbeff3228c39b5407c076a8b86
Gaurav Sawant <gauravsawant8217@gmail.com>
2026-01-07 13:49:55 +0530

    admin login template

--- NearFixApp/admin.py
+++ NearFixApp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+admin.site.site_header = "NearFix"
+admin.site.site_title = "Nearfix Admin Portal"
+admin.site.index_title = "Welcome to Nearfix Administration"
+
+# templates
+admin.site.login_template = "admin/login.html"
